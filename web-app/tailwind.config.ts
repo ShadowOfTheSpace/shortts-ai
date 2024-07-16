405a6e4192c5e73040ad6efeb6ec03fd87eb0052
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-16 17:41:12 +0300

    Add color palette

--- web-app/tailwind.config.ts
+++ web-app/tailwind.config.ts
@@ -4,6 +4,16 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        accent: "#8840b5",
+        background: "#EFE7F4",
+        divider: "#939393",
+        error: "#DC2626",
+        primary: "#472659",
+        secondary: "#B182CE",
+        tertiary: "#FFFFFF",
+        text: "#130C18",
+      },
       fontFamily: {
         roboto: ["var(--font-roboto)"],
       },
