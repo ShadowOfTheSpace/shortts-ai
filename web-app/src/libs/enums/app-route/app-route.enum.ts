9840405dff768247451d73d6687496a8e527187e
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-17 03:14:27 +0300

    Update AppRoute with new pages

--- web-app/src/libs/enums/app-route/app-route.enum.ts
+++ web-app/src/libs/enums/app-route/app-route.enum.ts
@@ -1,8 +1,11 @@
 const AppRoute = {
+  BILLING: "/dashboard/billing",
+  CREATE_VIDEO: "/dashboard/videos/create",
   ROOT: "/",
+  SETTINGS: "/dashboard/settings",
   SIGN_IN: "/auth/sign-in",
   SIGN_UP: "/auth/sign-up",
-  VIDEOS: "/videos",
+  VIDEOS: "/dashboard/videos",
 } as const;
 
 export { AppRoute };
