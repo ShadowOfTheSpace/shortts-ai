6fd4758fcb6c0ffe5d1806b711d4ffe163493cae
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-17 03:22:15 +0300

    Add Dashboard pages templates

---
+++ web-app/src/app/dashboard/settings/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+import { AppRoute } from "~/libs/enums/enums";
+import { auth } from "~/libs/modules/auth/auth";
+
+const SettingsPage: React.FC = async () => {
+  const user = await auth.getCurrentUser();
+
+  if (!user) {
+    redirect(AppRoute.SIGN_IN);
+  }
+
+  return <></>;
+};
+
+export default SettingsPage;
