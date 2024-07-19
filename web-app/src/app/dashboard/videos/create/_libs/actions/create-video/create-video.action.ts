c2367ce335b2a8bf116b4615641effd253ca4bd5
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-19 19:28:14 +0300

    Update create video action

--- web-app/src/app/dashboard/videos/create/_libs/actions/create-video/create-video.action.ts
+++ web-app/src/app/dashboard/videos/create/_libs/actions/create-video/create-video.action.ts
@@ -1,23 +1,48 @@
 "use server";
 import { parseWithZod } from "@conform-to/zod";
+import { redirect } from "next/navigation";
+import { AppRoute } from "~/_libs/enums/enums";
 import { type ActionErrorState } from "~/_libs/types/types";
 import { createVideoValidationSchema } from "~/_modules/videos/videos";
+import { videosDao } from "~/_modules/videos/videos.server-only";
 import { formatValidationError } from "~/_utils/utils";
 
 const createVideo = async (
   _error: ActionErrorState,
   formData: FormData
 ): Promise<ActionErrorState> => {
-  const parsedUserSignIn = parseWithZod(formData, {
+  const parsedCreateVideoParameters = parseWithZod(formData, {
     schema: createVideoValidationSchema,
   });
 
-  if (parsedUserSignIn.status !== "success") {
-    const errors = parsedUserSignIn.error;
-    return errors ? formatValidationError(errors) : ["Video creation failed."];
+  if (parsedCreateVideoParameters.status !== "success") {
+    const errors = parsedCreateVideoParameters.error;
+    return errors
+      ? formatValidationError(errors)
+      : ["Invalid video creation parameters."];
   }
 
-  return null;
+  const { colorPalette, style, topic, voice } =
+    parsedCreateVideoParameters.value;
+
+  const createdVideo = await videosDao.create(topic);
+  if (!createdVideo) {
+    return ["Video creation failed."];
+  }
+
+  try {
+    void (await videosDao.generate({
+      colorPalette,
+      id: createdVideo.id,
+      style,
+      quality: "HD",
+      voice,
+    }));
+  } catch (error: unknown) {
+    return ["Video creation failed."];
+  }
+
+  redirect(AppRoute.VIDEOS);
 };
 
 export { createVideo };
