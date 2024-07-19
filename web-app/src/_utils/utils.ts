b160b2c96ef0e8b964e846724eeb06d87d281b2d
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-19 03:29:51 +0300

    Add capitalizeFirstLetter utility function

--- web-app/src/_utils/utils.ts
+++ web-app/src/_utils/utils.ts
@@ -1,3 +1,4 @@
+export { capitalizeFirstLetter } from "./capitalize-first-letter/capitalize-first-letter.util";
 export { cn } from "./cn/cn.util";
 export { formatValidationError } from "./format-validation-errors/format-validation-errors.util";
 export { getUrlWithQueryParams } from "./get-url-with-query-params/get-url-with-query-params.util";
