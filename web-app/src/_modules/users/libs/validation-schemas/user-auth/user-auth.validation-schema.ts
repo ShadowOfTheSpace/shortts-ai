4569e3e9748feb2ee376d49651e271d018b16172
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-19 03:26:59 +0300

    Add trim to auth validation schema

--- web-app/src/_modules/users/libs/validation-schemas/user-auth/user-auth.validation-schema.ts
+++ web-app/src/_modules/users/libs/validation-schemas/user-auth/user-auth.validation-schema.ts
@@ -8,11 +8,13 @@ type UserAuth = {
 const userAuth = z.object<UserAuth>({
   email: z
     .string({ message: "Email is required." })
+    .trim()
     .email({ message: "Email is invalid." })
     .min(4, "Email is too short.")
     .max(70, "Email is too long."),
   password: z
     .string({ message: "Password is required." })
+    .trim()
     .min(5, "Password is to short.")
     .max(100, "Password is too long."),
 });
