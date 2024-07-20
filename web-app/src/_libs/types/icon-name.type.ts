9a9504b8ebebde2685770a66ba2f2d7ececc12fd
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-21 02:28:36 +0300

    Add new icons

--- web-app/src/_libs/types/icon-name.type.ts
+++ web-app/src/_libs/types/icon-name.type.ts
@@ -1,16 +1,22 @@
 type IconName =
   | "alert"
+  | "audioLines"
   | "check"
   | "chevronDown"
   | "chevronUp"
   | "chrome"
   | "clapperboard"
+  | "comment"
   | "creditCard"
+  | "film"
   | "folder"
+  | "hourglass"
+  | "images"
   | "loader"
   | "logOut"
   | "pause"
   | "play"
+  | "scrollText"
   | "settings"
   | "sparkles";
 
