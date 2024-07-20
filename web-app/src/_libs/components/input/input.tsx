814bfa51dd461c43e041eedc28e820e67175f6d1
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-21 02:25:08 +0300

    Add skeleton loading animation to Input component

--- web-app/src/_libs/components/input/input.tsx
+++ web-app/src/_libs/components/input/input.tsx
@@ -1,10 +1,14 @@
 import { cn } from "~/_utils/utils";
+import { Skeleton } from "../components";
 
 type Properties = {
   placeholder: string;
   defaultValue?: string;
   errors?: string[];
   inputClassName?: string;
+  skeletonClassName?: string;
+  isDisabled?: boolean;
+  isLoading?: boolean;
   key?: string;
   label?: string;
   name?: string;
@@ -16,6 +20,9 @@ const Input: React.FC<Properties> = ({
   defaultValue,
   errors,
   inputClassName,
+  skeletonClassName,
+  isDisabled,
+  isLoading,
   key,
   label,
   name,
@@ -24,7 +31,14 @@ const Input: React.FC<Properties> = ({
   return (
     <div className="flex flex-col gap-[2px] w-full">
       {label && <label className="font-bold text-[16px]">{label}</label>}
-      {rows ? (
+      {isLoading ? (
+        <Skeleton
+          className={cn(
+            "box-content border-transparent py-[8px] border rounded-[6px] h-[calc(1em*1.5)]",
+            skeletonClassName
+          )}
+        />
+      ) : rows ? (
         <textarea
           className={cn(
             "flex border-divider disabled:opacity-50 px-[12px] py-[8px] border rounded-[6px] w-full text-[16px] placeholder:text-divider focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary resize-none",
@@ -35,6 +49,7 @@ const Input: React.FC<Properties> = ({
           name={name}
           defaultValue={defaultValue}
           rows={rows}
+          disabled={isDisabled}
         />
       ) : (
         <input
@@ -46,6 +61,7 @@ const Input: React.FC<Properties> = ({
           key={key}
           name={name}
           defaultValue={defaultValue}
+          disabled={isDisabled}
         />
       )}
       <span
