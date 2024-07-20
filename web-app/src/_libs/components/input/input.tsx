7718e646e0ec11e830ce241732c7b44a57a824d4
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-07-20 16:47:03 +0300

    Add textarea variant to Input component

--- web-app/src/_libs/components/input/input.tsx
+++ web-app/src/_libs/components/input/input.tsx
@@ -8,6 +8,7 @@ type Properties = {
   key?: string;
   label?: string;
   name?: string;
+  rows?: number;
 };
 
 const Input: React.FC<Properties> = ({
@@ -18,20 +19,35 @@ const Input: React.FC<Properties> = ({
   key,
   label,
   name,
+  rows,
 }) => {
   return (
     <div className="flex flex-col gap-[2px] w-full">
       {label && <label className="font-bold text-[16px]">{label}</label>}
-      <input
-        className={cn(
-          "flex border-divider disabled:opacity-50 px-[12px] py-[8px] border rounded-[6px] w-full text-[16px] placeholder:text-divider focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary",
-          inputClassName
-        )}
-        placeholder={placeholder}
-        key={key}
-        name={name}
-        defaultValue={defaultValue}
-      />
+      {rows ? (
+        <textarea
+          className={cn(
+            "flex border-divider disabled:opacity-50 px-[12px] py-[8px] border rounded-[6px] w-full text-[16px] placeholder:text-divider focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary resize-none",
+            inputClassName
+          )}
+          placeholder={placeholder}
+          key={key}
+          name={name}
+          defaultValue={defaultValue}
+          rows={rows}
+        />
+      ) : (
+        <input
+          className={cn(
+            "flex border-divider disabled:opacity-50 px-[12px] py-[8px] border rounded-[6px] w-full text-[16px] placeholder:text-divider focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary",
+            inputClassName
+          )}
+          placeholder={placeholder}
+          key={key}
+          name={name}
+          defaultValue={defaultValue}
+        />
+      )}
       <span
         className={cn(
           "text-[12px] text-error min-h-[calc(1em*1.5)] ml-[5px]",
