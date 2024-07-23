f7ec41f1b8c174f9618d4a0932b573d956f3ee43
TeaChan <valkova.tanya17@gmail.com>
2024-07-23 04:21:13 +0300

    Add responsiveness to Hero section

--- web-app/src/app/_libs/components/hero-section/hero-section.tsx
+++ web-app/src/app/_libs/components/hero-section/hero-section.tsx
@@ -4,16 +4,16 @@ import { AppRoute } from "~/_libs/enums/enums";
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="before:top-0 before:absolute flex flex-col before:bg-background w-full before:w-full before:h-[90px]">
+    <section className="md:before:top-0 md:before:absolute flex flex-col md:before:bg-background w-full md:before:w-full md:before:h-[90px]">
       <div className="flex justify-center bg-background w-full">
-        <div className="flex justify-between items-center gap-x-[30px] px-[30px] w-full max-w-[1500px] h-[min(100vh,800px)]">
-          <div className="flex flex-col gap-y-[30px] basis-[55%]">
-            <h1 className="font-baloo font-bold text-[60px] text-balance leading-tight">
+        <div className="flex justify-between items-center gap-x-[30px] h-[calc(100dvh-80px-100px)] px-[20px] sm:px-[30px] w-full max-w-[1500px] lg:h-[min(100vh,800px)]">
+          <div className="flex flex-col gap-y-[30px] md:basis-[70%] lg:basis-[55%]">
+            <h1 className="font-baloo font-bold text-[40px] text-balance sm:text-[50px] lg:text-[60px] leading-tight">
               Transform your ideas into stunning{" "}
               <span className="text-accent">AI-generated</span> videos in a
               flash!
             </h1>
-            <p className="max-w-[500px] text-[20px] text-balance">
+            <p className="max-w-[500px] text-[16px] text-balance sm:text-[20px]">
               Shortts.ai simplifies video production with innovative AI tools,
               allowing you to effortlessly create compelling and impactful
               content that resonates with your audience.
@@ -31,11 +31,11 @@ const HeroSection: React.FC = () => {
             width={650}
             height={710}
             priority={true}
-            className="mt-[15px] min-w-[400px] max-w-full h-auto basis-[45%] self-start"
+            className="md:block hidden xl:mt-[15px] mb-[90px] xl:mb-0 w-full min-w-[300px] max-w-full h-auto self-center md:basis-[30%] lg:basis-[45%] xl:self-start"
           />
         </div>
       </div>
-      <Wave className="h-[140px] text-background" />
+      <Wave className="h-[90px] md:h-[140px] text-background" />
     </section>
   );
 };
