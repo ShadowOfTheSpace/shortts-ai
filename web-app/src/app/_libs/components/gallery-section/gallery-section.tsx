f1693c7715f4d939e10b71d26ba01df5e8977c77
TeaChan <valkova.tanya17@gmail.com>
2024-07-26 02:35:33 +0300

    Add responsiveness to Gallery section

--- web-app/src/app/_libs/components/gallery-section/gallery-section.tsx
+++ web-app/src/app/_libs/components/gallery-section/gallery-section.tsx
@@ -7,14 +7,14 @@ const GallerySection: React.FC = () => {
 
   return (
     <section className="flex flex-col justify-center items-center -mt-[200px] w-full">
-      <Wave className="-mb-[1px] h-[140px] text-primary scale-[-1]" />
+      <Wave className="-mb-[1px] h-[60px] md:h-[140px] text-primary scale-[-1]" />
       <div id="examples" className="flex justify-center bg-primary w-full">
-        <div className="flex flex-col items-center gap-y-[60px] px-[30px] pb-[30px] w-full max-w-[1500px]">
-          <h1 className="font-baloo font-bold text-[60px] text-background">
+        <div className="flex flex-col items-center gap-y-[30px] lg:gap-y-[60px] px-[30px] pb-[30px] w-full max-w-[1500px] overflow-x-hidden">
+          <h1 className="font-baloo font-bold text-[40px] text-background text-center sm:text-[50px] lg:text-[60px] leading-tight">
             Story. Illustrations. Effects.
           </h1>
           <ul
-            className="relative w-full overflow-x-hidden [--element-width:280px] [--element-height:490px] h-[var(--element-height)] [--scroll-time:30s] box-content py-[40px] [&>li>div]:hover:bg-primary/40 [&>*]:has-hover:hover:[animation-play-state:paused!important]"
+            className="relative w-[100vw] md:w-full md:overflow-x-hidden md:py-[40px] box-content [--element-width:200px] md:[--element-width:280px] [--element-height:350px] md:[--element-height:490px] h-[var(--element-height)] [--scroll-time:30s] [&>li>div]:has-hover:hover:bg-primary/40 [&>*]:has-hover:hover:[animation-play-state:paused!important] [&>*]:no-hover:active:[animation-play-state:paused!important]"
             style={
               {
                 "--element-count": videoCount,
@@ -37,7 +37,7 @@ const GallerySection: React.FC = () => {
           </ul>
         </div>
       </div>
-      <Wave className="-mt-[1px] h-[140px] text-primary" />
+      <Wave className="-mt-[1px] h-[60px] md:h-[140px] text-primary" />
     </section>
   );
 };
--- web-app/src/app/_libs/components/gallery-section/libs/components/gallery-item/gallery-item.tsx
+++ web-app/src/app/_libs/components/gallery-section/libs/components/gallery-item/gallery-item.tsx
@@ -6,11 +6,11 @@ type Properties = {
 const GalleryItem: React.FC<Properties> = ({ animationDelay, videoUrl }) => {
   return (
     <li
-      className="left-[max(100%,calc(var(--element-count)*var(--element-width)))] absolute rounded-[12px] w-[var(--element-width)] h-[var(--element-height)] transition-transform animate-infinity-scroll-left overflow-hidden aspect-[768/1344] has-hover:hover:scale-[1.1] [&>div]:has-hover:hover:!bg-transparent "
+      className="left-[max(100%,calc(var(--element-count)*var(--element-width)))] absolute rounded-[12px] transition-transform animate-infinity-scroll-left overflow-hidden aspect-[768/1344] w-[var(--element-width)] h-[var(--element-height)] has-hover:hover:scale-[1.1] [&>div]:has-hover:hover:!bg-transparent"
       style={{ animationDelay }}
     >
       <div className="absolute transition-colors size-full" />
-      <video muted autoPlay loop>
+      <video muted autoPlay loop className="pointer-events-none">
         <source src={videoUrl} type="video/webm" />
       </video>
     </li>
