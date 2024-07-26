beccd390a77cfc75fbd8cb1c5d48acc4dfb33cf8
TeaChan <valkova.tanya17@gmail.com>
2024-07-26 10:22:58 +0300

    Reorder gallery videos and add new one

--- web-app/src/app/_libs/components/gallery-section/libs/constants/gallery-videos/gallery-videos.constant.ts
+++ web-app/src/app/_libs/components/gallery-section/libs/constants/gallery-videos/gallery-videos.constant.ts
@@ -1,9 +1,10 @@
 const GALLERY_VIDEOS = [
-  "/videos/mirror-story.webm",
-  "/videos/dreamer-story.webm",
-  "/videos/elf-story.webm",
   "/videos/jazz-story.webm",
+  "/videos/fun-facts.webm",
   "/videos/forest-story.webm",
+  "/videos/elf-story.webm",
+  "/videos/dreamer-story.webm",
+  "/videos/mirror-story.webm",
 ] as const;
 
 export { GALLERY_VIDEOS };
